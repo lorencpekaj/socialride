de80be199c840684dd0ebbb9fda3ce59b91ad992
Lorenc Pekaj <zeelorenc@hotmail.com>
2019-10-08 20:55:41 +1100

    remove login as login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,18 +40,6 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Login as') }}</label>
-
-                            <div class="col-md-6">
-                                @error('type')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
